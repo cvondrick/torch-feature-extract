aa5c84750a4ce8d2f7f74cd5e523cf65b1db5911
Carl Vondrick <vondrick@mit.edu>
2016-01-23 14:01:04 -0500

    do not extract features if already done

--- extract_simple.lua
+++ extract_simple.lua
@@ -19,15 +19,11 @@ local image_dir = 'images'
 local out_dir = 'features'
 local prototxt = 'vgg16_places2/deploy.prototxt'
 local caffemodel = 'vgg16_places2/vgg16_places2.caffemodel'
-local layer_to_extract = 37 -- 39=fc8, 37=fc7, 31=pool5
-local batch_size = 2
+local layer_to_extract = 37
+local batch_size = 10
 local image_size = 224
 local gpu_device = 1 
 local mean_image = {105, 114, 116}
-local ext = 'h5' -- 'h5' or 't7'
-local force = true
-
-assert(image_dir:sub(image_dir:len()) ~= '/', 'image_dir should not end with /')
 
 -- load dependencies
 require 'cutorch'     -- CUDA tensors
@@ -56,35 +52,18 @@ print('extracting layer ' .. layer_to_extract)
 -- tensor to store RGB images on GPU 
 local input_images = torch.CudaTensor(batch_size, 3, image_size, image_size)
 
--- utility function to check if file exists
-function file_exists(name)
-  local f=io.open(name,"r")
-  if f~=nil then io.close(f) return true else return false end
-end
+-- keep track of the original filenames from input_images
+-- original_filenames[i] is the filename for the image input_images[i]
+local original_filenames = {}
+
+-- current index into input_images
+local counter = 1
 
 -- read all *.jpg files in the 'image_dir', and store in the array 'filepaths'
--- we recursively search the image dir
 local filepaths = {};
-local feat_paths = {}
-function find_images(searchdir)
-  print('searching ' .. searchdir)
-  -- search sub directories
-  for f in paths.iterdirs(searchdir) do
-    find_images(searchdir .. '/' .. f)
-  end
-  -- add files in this directory
-  for f in paths.iterfiles(searchdir) do
-    local f_full = searchdir .. '/' .. f
-    local feat_full = out_dir .. '/' .. f_full:sub(image_dir:len()+2) .. '.' .. ext
-
-    -- add to work queue if not done, or force is set
-    if force or not file_exists(feat_full) then
-      table.insert(filepaths, f_full)
-      table.insert(feat_paths, feat_full)
-    end
-  end
+for f in paths.files(image_dir, '.jpg') do
+  table.insert(filepaths, f)
 end
-find_images(image_dir) -- start recursion
 print('found ' .. #filepaths .. ' images')
 
 -- function to read image from disk, and do preprocessing
@@ -93,7 +72,7 @@ function load_caffe_image(impath)
   local im = image.load(impath)                 -- read image
   im = image.scale(im, image_size, image_size)  -- resize image
   im = im * 255                                 -- change range to 0 and 255
-  im = im:index(1,torch.LongTensor{3,2,1})      -- change RBB --> BGR
+  im = im:index(1,torch.LongTensor{3,2,1})      -- change BGR --> RGB
 
   -- subtract mean
   for i=1,3 do
@@ -104,7 +83,7 @@ function load_caffe_image(impath)
 end
 
 -- function to run feature extraction
-function extract_feat(size, last_id)
+function extract_feat()
   -- do forward pass of model on the images
   model:forward(input_images)
 
@@ -115,48 +94,33 @@ function extract_feat(size, last_id)
   feat = feat:float()
 
   -- save feature for item in batch
-  for i=1,size-1 do
-    -- make output directory if needed
-    paths.mkdir(paths.dirname(feat_paths[i+last_id-1]))
-
-    if ext == 'h5' then -- save hdf5 file
-      local hdf5_file = hdf5.open(feat_paths[i+last_id-1], 'w')
-      hdf5_file:write('feat', feat[i])
-      hdf5_file:close()
-
-    elseif ext == 't7' then -- save torch7 file
-      torch.save(feat_paths[i+last_id-1], feat[i])
-
-    else
-      assert(false, 'unknown filetype')
-    end
+  for i=1,counter-1 do
+    local hdf5_file = hdf5.open(out_dir .. '/' .. original_filenames[i] .. '.h5', 'w')
+    hdf5_file:write('feat', feat[i])
+    hdf5_file:close()
   end
 end
-
--- current index into input_images
-local counter = 1
-
--- last time we modified 
-local last_id = 1
 
 -- loop over each image
 for image_id, filepath in ipairs(filepaths) do
   xlua.progress(image_id, #filepaths) -- display progress
 
   -- read image and store on GPU
-  input_images[counter] = load_caffe_image(filepath)
+  input_images[counter] = load_caffe_image(image_dir .. '/' .. filepath)
+
+  -- keep track of original filename
+  original_filenames[counter] = filepath
 
   -- once we fill up the batch, extract, and reset counter
   counter = counter + 1
   if counter > batch_size then
-    extract_feat(counter, last_id) -- extract
-    counter = 1                    -- reset counter
-    last_id = image_id+1
-    input_images:zero()            -- for sanity, zero images
+    extract_feat()      -- extract
+    counter = 1         -- reset counter
+    input_images:zero() -- for sanity, zero images
   end
 end
 
 -- one last time for end of batch
 if counter > 1 then
-  extract_feat(counter, last_id)
+  extract_feat()
 end
